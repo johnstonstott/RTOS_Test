1e98536d6a3db36bb71039fc724aab031da531a6
Johnston Stott <johnstonstott@gmail.com>
2020-10-10 21:43:46 -0400

    Setting up serial monitor

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,20 @@
 
 #define LED_BUILTIN 2
 
+int count = 0;
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
+  count++;
+  Serial.println(count);
   // put your main code here, to run repeatedly:
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
+  delay(100);
   digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);
+  delay(100);
 }
